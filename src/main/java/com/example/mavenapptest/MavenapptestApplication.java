dfd6cafbbbd7ffbdfd9a61a59476e9f3603c70d9
kimin <kiminchan12341@gmail.com>
2023-05-08 22:31:11 +0900

    Initial commit

---
+++ src/main/java/com/example/mavenapptest/MavenapptestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenapptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenapptestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenapptestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mavenapptest/MavenapptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mavenapptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenapptestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
